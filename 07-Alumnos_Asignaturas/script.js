e85bff44eeabdeadb2599667b2b71b4bf159e491
IagoFP <133005241+IagoFP2004@users.noreply.github.com>
2024-10-15 13:05:32 +0200

    Muestra una tabla (no finalizado)

--- 07-Alumnos_Asignaturas/script.js
+++ 07-Alumnos_Asignaturas/script.js
@@ -1,7 +1,9 @@
-function procesarLista() {
-    var subjects = [];
-    var students = [];
+var data = []
+var subjects = [];
+var students = [];
 
+function procesarLista() {
+    
     var student = document.getElementById("students").value;
     var subject = document.getElementById("subjects").value;
 
@@ -10,13 +12,13 @@ function procesarLista() {
 
 
     students.forEach(student => {
-        let data = [];
+         data = [];
         data.push(student);
         subjects.forEach(subject=>{
             let number = (Math.random()*10).toFixed(2);
             data.push(number);
         })
-        console.log(data);
+        console.log("Datos procesados");
     });
 
    
@@ -24,6 +26,29 @@ function procesarLista() {
 }
 
 function mostrarDatos(){
+    let titulo =" ";
+    let alumnosT = '<th>'+"Alumnos"+'</th>';
+    let media = '<th>'+"Media"+'</th>';
+    let alumnos =" ";
 
+    for(i=0; i<subjects.length; i++){
+       titulo+= `<th>${subjects[i]}</th>`;
+    }
 
+    for(i=0; i<students.length; i++){
+
+        alumnos+=`<tr><td>${students[i]}</td</tr>`;
+
+    }
+
+ document.getElementById("tabla").innerHTML+= ` 
+ <table border="solid">
+    <tr>
+        ${alumnosT+titulo+media}
+    </tr>
+    <tr>
+        ${alumnos};
+    </tr>
+    </table>
+    `
 }
