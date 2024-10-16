a0d245789d600b9ff41193e4fde17f678e809242
IagoFP <iagofranciscoperez123@gmail.com>
2024-10-16 19:38:31 +0200

    aun no se  muestra la tabla

--- 08-hundir_la_flota/script.js
+++ 08-hundir_la_flota/script.js
@@ -11,24 +11,31 @@ var tablero = [
 function disparar() {
     var x = document.getElementById("x").value;
     var y = document.getElementById("y").value;
-    
-    tablero.forEach(fila =>{
-        fila.forEach(casilla=>{
-            document.getElementById("tabla").innerHTML += `<table border="solid"><tr>${fila}</tr>`
-        })
-    })
-    
-    
-    
-    
-    for(i=0; i<tablero.length; i++){
-        for(j=0; j<tablero[i].length; j++){
-           if(tablero[x][y]===1){
-            console.log("true");
-           }else{
-            console.log("false");
+
+    mostrarTabla();
+
+    for (i = 0; i < tablero.length; i++) {
+        for (j = 0; j < tablero[i].length; j++) {
+            if (tablero[x][y] === 1) {
+                console.log("true");
+
+            } else {
+                console.log("false");
+
+            }
+        }
+    }
+}
+
+function mostrarTabla() {
+    tabla=document.getElementById("tabla");
+    tabla.innerHTML=" ";
+    for (var i = 0; i < tablero.length; i++) {
+        for (var j = 0; j < tablero[i].length; j++) {
+            var celda = document.createElement("div"); 
+
+            tabla.appendChild(fila);
             
-           }
         }
     }
 }
