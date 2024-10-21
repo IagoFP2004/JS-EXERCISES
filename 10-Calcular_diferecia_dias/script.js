47b75313e0a32c1c239e457681efc0eec8b1f976
IagoFP <133005241+IagoFP2004@users.noreply.github.com>
2024-10-21 12:36:16 +0200

    Tenemos los dias almacenados
    Paso siguiente pasar todo a dias para poder restarlo

---
+++ 10-Calcular_diferecia_dias/script.js
@@ -0,0 +1,18 @@
+//Convertimos los dias en un array con el dia mes y año
+const date1 = document.getElementById("fecha1").value.split("/");
+const date2 = document.getElementById("fecha2").value.split("/");
+
+function calcularDiferenciaFechas()
+{
+    //Almacenamos los valores de los arrays para poder operar con ellos
+    let day1 = date1[0];
+    let month1 = date1[1];
+    let year1 = date1[2];
+
+    let day2 = date2[0];
+    let month2 = date2[1];
+    let year2 = date2[2];
+
+    
+    
+}
